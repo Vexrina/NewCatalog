68351de840fd16616eaa1604b4d878ba2222d13e
Konstantin Schavlev <gameofsteam.ru@gmail.com>
2023-01-30 16:08:15 +0300

    creating main.py correctly

--- app/main.py
+++ app/main.py
@@ -1,19 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from pathlib import Path
+import uvicorn
 
-app = FastAPI()
-app.mount(
-    '/static/',
-    StaticFiles(directory=Path(__file__).parent.parent/'static'),
-    name='static',
-)
-
-templates = Jinja2Templates(directory='templates')
-
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse(
-        "homepage.html", {"request":request}
+if __name__ == '__main__':
+    uvicorn.run(
+        'app:app',
+        host='localhost',
+        port=8080,
+        reload=True,
     )
