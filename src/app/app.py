28f7ca12cbe6af7dbb23472ca7c8be251e002192
Konstantin Schavlev <gameofsteam.ru@gmail.com>
2023-02-11 20:16:42 +0300

    idk what's new. main.py have to be rewritten

--- src/app/app.py
+++ src/app/app.py
@@ -1,18 +1,16 @@
 from typing import Any
 # import generate_page
 
-from fastapi import FastAPI, Request, HTTPException, Depends
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from pathlib import Path
 
-from sqlalchemy.orm import Session
-
-from src.backend import crud, models,  schemas
-from src.backend.database import SessionLocal, engine
+from src.backend.database import SessionLocal
 
 app = FastAPI()
+
 
 def get_db():
     db = SessionLocal()
@@ -21,54 +19,65 @@ def get_db():
     finally:
         db.close()
 
+
 app.mount(
     '/static/',
-    StaticFiles(directory=Path(__file__).parent.parent/'static'),
+    StaticFiles(directory=Path(__file__).parent.parent / 'static'),
     name='static',
 )
 
-templates = Jinja2Templates(directory=Path(__file__).parent.parent/'pages')
+templates = Jinja2Templates(directory=Path(__file__).parent.parent / 'pages')
+
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request) -> Any:
     # generate_page.generate_homepage()
-    context = {'request':request}
+    context = {'request': request}
     return templates.TemplateResponse("homepage.html", context)
+
 
 @app.get('/cpu/')
 async def cpus(request: Request):
     # generate_page.generate_category_page('cpu')
-    context = {'request':request}
+    context = {'request': request}
     return templates.TemplateResponse("cpu.html", context)
+
 
 @app.get('/gpu/')
 async def cpus(request: Request):
-    return {'request':'gpu'}
+    return {'request': 'gpu'}
+
 
 @app.get('/ram/')
 async def cpus(request: Request):
-    return {'request':'ram'}
+    return {'request': 'ram'}
+
 
 @app.get('/motherboard/')
 async def cpus(request: Request):
-    return {'request':'motherboard'}
+    return {'request': 'motherboard'}
+
 
 @app.get('/storage/')
 async def cpus(request: Request):
-    return {'request':'storage'}
+    return {'request': 'storage'}
+
 
 @app.get('/power_unit/')
 async def cpus(request: Request):
-    return {'request':'power_unit'}
+    return {'request': 'power_unit'}
+
 
 @app.get('/cpu_fan/')
 async def cpus(request: Request):
-    return {'request':'cpu_fan'}
+    return {'request': 'cpu_fan'}
+
 
 @app.get('/shopping_cart/')
 async def cpus(request: Request):
-    return {'request':'shopping_cart'}
+    return {'request': 'shopping_cart'}
+
 
 @app.get('/profile/')
 async def cpus(request: Request):
-    return {'request':'profile'}
+    return {'request': 'profile'}
