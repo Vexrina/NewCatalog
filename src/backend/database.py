cb1a0f7dcffcf7768b31e71d88d45de785ce55d8
Konstantin Schavlev <gameofsteam.ru@gmail.com>
2023-02-19 15:53:10 +0300

    Deleted commentaries

--- src/backend/database.py
+++ src/backend/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./newcatalog_db.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///C:\\Users\\Vexrina\\Desktop\\projects\\NewCatalog\\src\\backend\\newcatalog_db.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
