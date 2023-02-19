2488050be1815e8668c8024394fe7992d638cd2b
Konstantin Schavlev <gameofsteam.ru@gmail.com>
2023-02-19 15:54:10 +0300

    create function for taking data from db

--- src/app/generate_page.py
+++ src/app/generate_page.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
 from jinja2 import Environment, FileSystemLoader
 import os
-from typing import List, Type
+from typing import List, Type, Any
 from src.backend.models import cpu_models
+from src.backend.cruds import cpu_crud
+
+
+directory = Path(__file__).parent.parent
+
 
 def take_header_items() -> list[dict]:
     header_items = []
 
-    for __, __, files in os.walk('./static/img/svg/'):
+    for __, __, files in os.walk(directory/'static/img/svg/'):
         for filename in files:
             if filename != 'red_splash.svg':
                 header_items.append(
@@ -19,10 +26,68 @@ def take_header_items() -> list[dict]:
     return header_items
 
 
+def take_db_items(filters: dict | None, query: str = ''):
+    if filters is None:
+        filters = {}
+    match query:
+        case 'cpu':
+            if len(filters) == 0:
+                cpus = cpu_crud.get_cpus()
+                data = []
+                for cpu in cpus:
+                    temp = {
+                        'brand': cpu.brand,
+                        'model': cpu.model,
+                        'image': cpu.image,
+                        'specs': cpu_crud.get_cpu_specs_by_cpuid(cpu.uuid)
+                    }
+                    data.append(temp)
+                return data
+            elif 'brand' in filters and 'model' not in filters:
+                cpus = cpu_crud.get_cpu_by_brand(filters['brand'])
+                data = []
+                for cpu in cpus:
+                    temp = {
+                        'brand': cpu.brand,
+                        'model': cpu.model,
+                        'image': cpu.image,
+                        'specs': cpu_crud.get_cpu_specs_by_cpuid(cpu.uuid)
+                    }
+                    data.append(temp)
+                return data
+            elif 'model' in filters:
+                cpu = cpu_crud.get_cpu_by_model(filters['model'])
+                data = {
+                    'brand': cpu.brand,
+                    'model': cpu.model,
+                    'image': cpu.image,
+                    'specs': cpu_crud.get_cpu_specs_by_cpuid(cpu.uuid)
+                }
+                return data
+            else:
+                raise ValueError('Not implemented')
+        case 'gpu':
+            pass
+        case 'ram':
+            pass
+        case 'mb':
+            pass
+        case 'ssd':
+            pass
+        case 'hdd':
+            pass
+        case 'pu':
+            pass
+        case 'fan':
+            pass
+        case _:
+            pass
+
+
 def generate_homepage() -> None:
     header_items = take_header_items()
 
-    file_loader = FileSystemLoader('templates')
+    file_loader = FileSystemLoader(directory/'templates')
     env = Environment(loader=file_loader)
 
     rendered = env.get_template('homepage.html').render(
@@ -30,24 +95,17 @@ def generate_homepage() -> None:
 
     filename = 'homepage.html'
 
-    with open(f'./pages/{filename}', 'w', encoding='UTF-8') as f:
+    with open(directory/'pages'/filename, 'w', encoding='UTF-8') as f:
         f.write(rendered)
 
 
-def generate_category_page(query_result: list[Type[cpu_models.Cpus]], category: str) -> None:
+def generate_category_page(category: str) -> None:
     header_items = take_header_items()
 
-    file_loader = FileSystemLoader('templates')
+    file_loader = FileSystemLoader(directory/'templates')
     env = Environment(loader=file_loader)
 
-
-    data = []
-    for link in links:
-        data.append(get_data(link))
-
-    for item in data:
-        if len(item)==0:
-            data.remove(item)
+    data = take_db_items(query='cpu', filters=None)
 
     rendered = env.get_template('category.html').render(
         header_items=header_items,
@@ -59,5 +117,3 @@ def generate_category_page(query_result: list[Type[cpu_models.Cpus]], category: 
         f.write(rendered)
     print('Html is rendered')
 
-
-# generate_category_page('CPU')
